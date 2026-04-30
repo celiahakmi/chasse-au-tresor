9626e032cc82614a9ce6dfc57ab8309c1cd0429f
Célia Hakmi <celiahakmi@gmail.com>
2026-04-30 19:56:27 +0200

    ajout d'une classe Statistique

---
+++ src/Statistique.java
@@ -0,0 +1,10 @@
+public class Statistique {
+    
+    public static int nbTresors= 0;
+    public static int nbDeplacements=0;
+
+    public static void afficher(){
+        System.out.println("Tresors trouvés: " + nbTresors);
+        System.out.println("Deplacements: " + nbDeplacements);
+    }
+}
