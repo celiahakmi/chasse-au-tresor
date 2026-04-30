3b5013cd1fc4c5217aa81e18fed453f074fbced7
Célia Hakmi <celiahakmi@gmail.com>
2026-04-30 20:03:12 +0200

    ajout de la classe héritage

---
+++ src/ExplorateurAvance.java
@@ -0,0 +1,11 @@
+public class ExplorateurAvance extends Explorateur{
+
+    public ExplorateurAvance(Terrain t, int l, int c) {
+        super(t, l, c);
+    }
+
+    public void agir() {
+        super.agir();
+    }
+
+}
