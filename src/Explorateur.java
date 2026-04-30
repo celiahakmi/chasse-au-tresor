34a0fc298139d55bf1e4f5e2e04ec535c12d71af
Célia Hakmi <celiahakmi@gmail.com>
2026-04-30 20:01:11 +0200

    modif et correction de bugs

--- src/Explorateur.java
+++ src/Explorateur.java
@@ -5,9 +5,24 @@ public class Explorateur extends Agent{
     }
 
     public void agir(){
-        int newl=lig+(int)(Math.random()*3-1);
-        int newc=col+(int)(Math.random()*3-1);
-        seDeplacer(newl,newc);
-        System.out.println("Explorateur se déplace en (" + newl + "," + newc + ")");
+    int targetL = 8;
+    int targetC = 8;
+
+    int newL = lig;
+    int newC = col;
+
+    if (lig < targetL) newL++;
+    else if (lig > targetL) newL--;
+
+    if (col < targetC) newC++;
+    else if (col > targetC) newC--;
+
+    seDeplacer(newL, newC);
+
+    System.out.println("Explorateur → (" + newL + "," + newC + ")");
+
+    if (newL == targetL && newC == targetC) {
+        System.out.println("TRÉSOR TROUVÉ !");
+    }
     }
 }
