7f3a9de69b61df0262ae211c60e3e4e7c860fa83
Célia Hakmi <celiahakmi@gmail.com>
2026-04-30 19:55:30 +0200

    ajout d'une classe Tempete

---
+++ src/Tempete.java
@@ -0,0 +1,30 @@
+/**
+ * AID
+ * Classe représentant une tempête dans la simulation.
+ * Une tempête peut déplacer des ressources et perturber les agents.
+ */
+public class Tempete {
+
+    private int intensite;
+
+    public Tempete(int intensite) {
+        this.intensite = intensite;
+    }
+
+    public int getIntensite() {
+        return intensite;
+    }
+
+    /**
+     * Applique les effets de la tempête sur la simulation.
+     */
+    public void agir(Simulation sim) {
+
+        System.out.println("\nUne tempête frappe la mer! Intensité = " + intensite);
+
+        sim.deplacerRessourcesAleatoirement(intensite);
+
+        sim.bloquerAgents(intensite);
+
+    }
+}
