d6b6458de82c35f7578991f9b7c3c984709234c7
Célia Hakmi <celiahakmi@gmail.com>
2026-04-30 18:47:20 +0200

    ajout du main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main (String []args){
+        Terrain terrain= new Terrain (10,10);
+        Simulation simulation= new Simulation(terrain);
+
+        simulation.run(5);
+    }
+}
