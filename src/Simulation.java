a671703f08c2419932eab0ae91fa0d4ffe12a531
Célia Hakmi <celiahakmi@gmail.com>
2026-05-01 17:05:15 +0200

    maj de la simulation

--- src/Simulation.java
+++ src/Simulation.java
@@ -9,49 +9,51 @@ public class Simulation {
         this.terrain = terrain;
         this.agents = new ArrayList<>();
         //les agents
-        agents.add(new Explorateur(terrain, 0, 0));
         agents.add(new Explorateur(terrain, 1, 1));
+        agents.add(new Explorateur(terrain, 2, 2));
         //les ressources sur le terrain
         terrain.setCase(8, 8, new Tresor(100));
         terrain.setCase(2,2, new Algue(5));
         terrain.setCase(5,7, new Algue(2));
     }
 
-    public void step() {
-        System.out.println("=== ETAPE DE SIMULATION ===");
+    public void step(int tour) {
+        System.out.println("\n=== TOUR " +tour +" ===");
         for (Agent a : agents) {
-            a.agir();
+            try{
+                a.agir();
+            } catch (HorsMerException e){
+                System.out.println("ATTENTION : " + e.getMessage());
+            }
         }
+        ArrayList<Ressource> listeRessources=terrain.lesRessources();
+        for (Ressource r: listeRessources){
+            if (r instanceof Aquatique){
+                ((Aquatique)r).evoluer();
+            }
+        }  
+        terrain.afficher(5);  
     }
 
-    public void run(int steps) {
-        for (int i = 0; i < steps; i++) {
-            step();
+    public void run(int nbtours) {
+        for (int i = 0; i < nbtours; i++) {
+            step(i);
+            if (Math.random()<0.2){
+                declencherTempete();
+            }
         }
+        Statistique.getInstance().afficher();
     }
 
-    //déplacement des ressources
-    public void deplacerRessourcesAleatoirement(int intensite) {
-
-        for (int i = 0; i < ressources.size(); i++) {
-
-            Ressource r = ressources.get(i);
-
-            int x = (int)(Math.random() * 5 + 1);
-            int y = (int)(Math.random() * 5 + 1);
-
-            terrain.setCase(x, y, r);
-        }
-
-        System.out.println("Les ressources ont été déplacées par la tempête");
-    }
-
-    //blocage agents
-    public void bloquerAgents(int intensite) {
-
-        for (Agent a : agents) {
-            if (Math.random() < 0.3 * intensite) {
-                System.out.println("ATTENTION " + a + " est bloqué par la tempête !");
+    public void declencherTempete(){
+        System.out.println("Une tempête éclate, les courants déplacent les ressources...");
+        ArrayList<Ressource> ressources= terrain.lesRessources();
+        for (Ressource r: ressources){
+            terrain.viderCase(r.getLigne(),r.getColonne());
+            int nl=(int)(Math.random()*terrain.nbLignes)+1;
+            int nc= (int)(Math.random()*terrain.nbColonnes)+1;
+            if (terrain.caseEstVide(nl, nc)){
+                terrain.setCase(nl,nc,r);
             }
         }
     }
