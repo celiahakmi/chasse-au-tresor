366029030590e61f55888bb7ea68094b319a5527
Célia Hakmi <celiahakmi@gmail.com>
2026-05-01 19:32:43 +0200

    ajout de l'en tête

--- src/Algue.java
+++ src/Algue.java
@@ -1,3 +1,9 @@
+/**
+ * HAKMI CELIA
+ * Classe représentant une ressource de type Algue.
+ * Implémente l'interface Aquatique pour marquer son appartenance au milieu marin.
+ */
+
 public class Algue extends Ressource implements Aquatique {
 
     public Algue (int quantite){
