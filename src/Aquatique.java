d4d07e409a91f7b5fe878ee1a9b75c02c0c64f14
Célia Hakmi <celiahakmi@gmail.com>
2026-05-01 19:32:52 +0200

    ajout de l'en tête

--- src/Aquatique.java
+++ src/Aquatique.java
@@ -1,3 +1,9 @@
+/**
+ * HAKMI CELIA
+ * Interface définissant les propriétés des éléments vivant en mer.
+ * Utilisée pour marquer les ressources comme les algues.
+ */
+
 public interface Aquatique {
     void evoluer();
 }
