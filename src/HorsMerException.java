b7422aecd45c3b0260e5b9f2d936c3954f26ddf9
Célia Hakmi <celiahakmi@gmail.com>
2026-05-01 19:28:26 +0200

    ajout de l'en tête

--- src/HorsMerException.java
+++ src/HorsMerException.java
@@ -1,3 +1,8 @@
+/**
+ * HAKMI CELIA
+ * Exception personnalisée gérant les tentatives de sortie des limites du terrain.
+ */
+
 public class HorsMerException extends Exception {
     
     public HorsMerException(String message){
