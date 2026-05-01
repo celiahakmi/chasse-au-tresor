2f444da7e21ebc655931451b25faf8c22ea9d7e2
Célia Hakmi <celiahakmi@gmail.com>
2026-05-01 13:13:32 +0200

    transformation Statistique en singleton et ajout méthode affichage grille

--- src/Statistique.java
+++ src/Statistique.java
@@ -1,10 +1,21 @@
 public class Statistique {
     
+    private static Statistique instance=null;
     public static int nbTresors= 0;
     public static int nbDeplacements=0;
 
+    private Statistique(){}
+
+    public static Statistique getIstance(){
+        if(instance==null){
+            instance=new Statistique();
+        }
+        return instance;
+    }
+
     public static void afficher(){
+        System.out.println("\n--- STATISTIQUES FINALES ---");
         System.out.println("Tresors trouvés: " + nbTresors);
-        System.out.println("Deplacements: " + nbDeplacements);
+        System.out.println("Deplacements totaux : " + nbDeplacements);
     }
 }
