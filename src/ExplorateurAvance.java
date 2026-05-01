e83b951bfa0bb6f510ea4f74e2616725befa3551
Célia Hakmi <celiahakmi@gmail.com>
2026-05-01 19:33:01 +0200

    ajout de l'en tête

--- src/ExplorateurAvance.java
+++ src/ExplorateurAvance.java
@@ -1,3 +1,9 @@
+/**
+ * HAKMI CELIA
+ * Classe représentant un explorateur expérimenté.
+ * Hérite d'Explorateur et possède des capacités de déplacement ou de détection accrues.
+ */
+
 public class ExplorateurAvance extends Explorateur{
 
     public ExplorateurAvance(Terrain t, int l, int c) {
