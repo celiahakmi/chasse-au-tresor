cbb0c07be0c357255d0a18927df01203c0521db3
Célia Hakmi <celiahakmi@gmail.com>
2026-05-01 17:49:32 +0200

    ajout constructeur de copie

--- src/Tresor.java
+++ src/Tresor.java
@@ -3,4 +3,8 @@ public class Tresor extends Ressource {
     public Tresor(int valeur){
         super("TRESOR", valeur);
     }
+
+    public Tresor(Tresor t){
+        super("TRESOR", t.getQuantite());
+    }
 }
