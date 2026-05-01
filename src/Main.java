6b5edcc6e9612f3055cc38c35bf57fe82a919502
Célia Hakmi <celiahakmi@gmail.com>
2026-05-01 19:33:09 +0200

    ajout de l'en tête

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+/**
+ * HAKMI CELIA
+ * Point d'entrée du programme.
+ * Initialise le terrain, crée les agents et lance la simulation.
+ */
+
 public class Main{
     public static void main (String []args){
         Terrain terrain= new Terrain (10,10);
